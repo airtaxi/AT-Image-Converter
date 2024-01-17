2547429acf3672c266d785ae62514ff4983b5d18
Howon Lee <kck4156@kagamine-rin.com>
2024-01-17 17:42:26 +0900

    Feature: Update checker

--- AT Image Converter/App.xaml.cs
+++ AT Image Converter/App.xaml.cs
@@ -1,11 +1,53 @@
 ﻿using ImageConverterAT.Helpers;
 using Microsoft.UI.Xaml;
+using System.Reflection;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Toolkit.Uwp.Notifications;
 
 namespace ImageConverterAT;
 
 public partial class App : Application
 {
-	private Window _window;
+    private const int UpdateCheckIntervalInMinutes = 10;
+    private static readonly Timer UpdateCheckTimer;
+
+    static App()
+    {
+        UpdateCheckTimer = new(UpdateCheckTimerCallback, null, (int)TimeSpan.FromMinutes(UpdateCheckIntervalInMinutes).TotalMilliseconds, Timeout.Infinite);
+    }
+
+    private const string UpdateAvailableTitle = "Update Available";
+    private static async void UpdateCheckTimerCallback(object state) => await CheckForUpdateAsync();
+
+    private static async Task CheckForUpdateAsync()
+    {
+        try
+        {
+            var url = "https://raw.githubusercontent.com/airtaxi/AT-Image-Converter/master/latest";
+            var remoteVersionString = await HttpHelper.GetContentFromUrlAsync(url);
+            if (string.IsNullOrEmpty(remoteVersionString)) return;
+
+            var localVersion = Assembly.GetExecutingAssembly().GetName().Version;
+            var remoteVersion = new Version(remoteVersionString);
+            if (localVersion >= remoteVersion) return;
+
+            var configurationKey = "versionChecked" + remoteVersionString;
+            var hasNotificationShownForRemoteVersion = Configuration.GetValue<bool?>(configurationKey) ?? false;
+            if (hasNotificationShownForRemoteVersion) return;
+            Configuration.SetValue(configurationKey, true);
+
+            var builder = new ToastContentBuilder()
+            .AddText(UpdateAvailableTitle)
+            .AddText($"A new version ({remoteVersion}) is available.\nDo you want to download it?")
+            .AddArgument("versionString", remoteVersionString);
+            builder.Show();
+        }
+        finally { UpdateCheckTimer.Change((int)TimeSpan.FromMinutes(UpdateCheckIntervalInMinutes).TotalMilliseconds, Timeout.Infinite); }
+    }
+
+    private Window _window;
 
 	public App()
 	{
@@ -13,9 +55,10 @@ public partial class App : Application
 		FileExplorerContextMenuHelper.TryAddProgramToSendToContextMenu();
 	}
 
-	protected override void OnLaunched(LaunchActivatedEventArgs args)
+	protected override async void OnLaunched(LaunchActivatedEventArgs args)
 	{
 		_window = new MainWindow();
 		_window.Activate();
-	}
+        await CheckForUpdateAsync();
+    }
 }
---
+++ AT Image Converter/Helpers/HttpHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ImageConverterAT.Helpers;
+
+public static class HttpHelper
+{
+    private static readonly HttpClient SharedHttpClient = new();
+    public static async Task<string> GetContentFromUrlAsync(string url)
+    {
+        try
+        {
+            HttpResponseMessage response = await SharedHttpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+        catch (HttpRequestException) { return null; }
+    }
+}
