5bc47a6f437571931df7905792c33bbb1661f4e5
Howon Lee <kck4156@kagamine-rin.com>
2024-12-17 14:58:32 +0900

    Using Program.cs

---
+++ AT Image Converter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using Microsoft.UI.Dispatching;
+using Microsoft.UI.Xaml;
+
+namespace ImageConverterAT;
+
+public class Program
+{
+    [STAThread]
+    static int Main(string[] args)
+    {
+        Application.Start((p) =>
+        {
+            var context = new DispatcherQueueSynchronizationContext(
+                DispatcherQueue.GetForCurrentThread());
+            SynchronizationContext.SetSynchronizationContext(context);
+            _ = new App();
+        });
+
+        return 0;
+    }
+}
